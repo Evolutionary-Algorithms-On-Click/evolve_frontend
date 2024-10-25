d2e1b08a9de7cf3074568c082b5ddf6e46fb4d1f
Ashrockzzz2003 <ashrockzzz2003@gmail.com>
2024-10-25 19:19:05 +0530

    Weights UI done.

--- src/app/create/page.js
+++ src/app/create/page.js
@@ -2,10 +2,18 @@
 
 import { useState } from "react";
 import { algorithmData } from "../_data/algorithms"
+import { individualData } from "../_data/individual";
 
 export default function CreateInstance() {
     const [currentStep, setCurrentStep] = useState(1)
+
+
     const [algo, setAlgo] = useState(null)
+
+    const [parameters, setParameters] = useState([])
+    const [tempParamWeight, setTempParamWeight] = useState(null)
+
+    const [indGen, setIndGen] = useState(null)
 
     return (
         <main className="items-center justify-items-center min-h-screen font-[family-name:var(--font-geist-mono)] p-8">
@@ -20,13 +28,35 @@ export default function CreateInstance() {
                 {/* 
                     Section will have a div representing selected values.
                 */}
-                <div className="flex flex-col items-start border border-gray-400 rounded-2xl p-4 min-w-[15%]">
-                    <h3 className="text-xl font-bold">Configuration</h3>
+                <div className="flex flex-col items-start border border-gray-400 rounded-2xl p-4 min-w-16 h-fit">
+                    <h3 className="text-xl font-bold">Config Summary</h3>
                     <div className="flex flex-col">
                         <div className="mt-4">
                             <h4 className="text-lg font-bold">Algorithm</h4>
                             <code className="bg-foreground p-1 rounded-lg text-background">{algo || "None"}</code>
                         </div>
+
+
+                        {currentStep >= 2 && (
+                            <div className="mt-4">
+                                <h4 className="text-lg font-bold">Weights</h4>
+                                {parameters.length > 0 ? (
+                                    <table className="w-full text-center">
+                                        <tbody>
+                                            {parameters.map((param, index) => (
+                                                <tr key={index} className={index % 2 === 0 ? "bg-gray-100" : "bg-white"}>
+                                                    <td className="border-b border-gray-300 p-2">
+                                                        <p className="font-bold">{param + ".0"}</p>
+                                                        <p className="font-extralight">dim_{index + 1}</p>
+                                                    </td>
+                                                </tr>
+                                            ))}
+                                        </tbody>
+                                    </table>
+                                ) : (<p>None</p>)}
+                            </div>
+                        )}
+
                     </div>
                 </div>
 
@@ -59,6 +89,78 @@ export default function CreateInstance() {
                         {currentStep >= 2 && (
                             <div className="mt-4">
                                 <h4 className="text-lg font-bold mb-8">Step 2: Parameters to optimize (weights)</h4>
+                                {/* Parameter {'name': 'paramName', 'weight': <1.0/-1.0> (maximize/minimize)} 
+                                    UI split into two halves, add param to right. left is a list of added params.
+                                */}
+
+                                <div className="grid grid-cols-2 gap-8 justify-items-stretch align-top">
+                                    <div className="flex flex-col border border-foreground border-solid p-4 rounded-3xl w-full h-fit">
+                                        <h5 className="text-lg font-bold mb-3">Add Parameter</h5>
+
+                                        <div className="flex gap-0 items-center my-2">
+                                            <button onClick={(e) => {
+                                                e.preventDefault()
+                                                setTempParamWeight(1.0)
+                                            }} className={"p-1 rounded-l-xl w-full border border-foreground" + (tempParamWeight === 1.0 ? " bg-foreground text-background" : "")}>
+                                                <p>Maximize</p>
+                                                <p className="font-extralight">(weight = +1.0)</p>
+                                            </button>
+                                            <button onClick={(e) => {
+                                                e.preventDefault()
+                                                setTempParamWeight(-1.0)
+                                            }} className={"p-1 rounded-r-xl w-full border border-foreground" + (tempParamWeight === -1.0 ? " bg-foreground text-background" : "")}>
+                                                <p>Maximize</p>
+                                                <p className="font-extralight">(weight = -1.0)</p>
+                                            </button>
+                                        </div>
+
+                                        <button onClick={(e) => {
+                                            e.preventDefault()
+                                            if (tempParamWeight === null) {
+                                                alert("Please select whether to maximize/minimize.")
+                                                return
+                                            }
+                                            setParameters([...parameters, tempParamWeight])
+                                            setTempParamWeight(null)
+                                        }} className="bg-foreground text-background p-1 rounded-xl">Add</button>
+                                    </div>
+                                    <div className="flex flex-col">
+                                        <h5 className="text-lg font-bold">Optimization Parameters</h5>
+
+                                        <table className="w-full text-center">
+                                            <thead>
+                                                <tr>
+                                                    <th className="border-b-2 border-gray-300 p-2">Weight</th>
+                                                    <th className="border-b-2 border-gray-300 p-2">Actions</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                {parameters.map((param, index) => (
+                                                    <tr key={index} className={index % 2 === 0 ? "bg-gray-100" : "bg-white"}>
+                                                        <td className="border-b border-gray-300 p-2">
+                                                            <p className="font-bold">{param + ".0"}</p>
+                                                            <p className="font-extralight">{`dim_${index + 1}`}</p>
+                                                        </td>
+                                                        <td className="border-b border-gray-300 p-2">
+                                                            <button onClick={(e) => {
+                                                                e.preventDefault()
+                                                                setParameters(parameters.filter((_, i) => i !== index))
+                                                            }} className="bg-foreground text-background p-1 rounded-lg">Remove</button>
+
+                                                        </td>
+                                                    </tr>
+                                                ))}
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                        )}
+
+                        {parameters.length > 0 && (
+                            <div className="mt-4">
+                                <h4 className="text-lg font-bold mb-8">Step 3: Choose an individual generator function.</h4>
+                                {/* grid: each element has a name and description */}
                             </div>
                         )}
                     </form>
--- src/app/page.js
+++ src/app/page.js
@@ -13,7 +13,7 @@ export default function Home() {
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+            className="rounded-full border border-solid border-black/[.08] transition-colors flex items-center justify-center bg-background text-foreground gap-2 hover:bg-[#383838] hover:text-background text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
             href="/create"
           >
             <Image
@@ -26,7 +26,7 @@ export default function Home() {
             Get Started
           </a>
           <p
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44 flex-col"
+            className="rounded-full transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44 flex-col bg-foreground text-background cursor-not-allowed"
           >
             Read our docs
             <span className="text-xs font-extralight">{"coming soon"}</span>
