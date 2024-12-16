da4dacccb8c2fb4d68cf58d52e96d4d348a4712b
Ashrockzzz2003 <ashrockzzz2003@gmail.com>
2024-12-16 12:06:30 +0530

    Modularize choose algorithm.

---
+++ src/app/new/page.js
@@ -0,0 +1,57 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import Loader from "../_components/Loader";
+import { ChooseAlgo } from "./_components/chooseAlgorithm";
+import { algorithmData } from "../_data/algorithms";
+import ChooseWeights from "./_components/chooseWeights";
+
+export default function NewRunner() {
+    const [currentStep, setCurrentStep] = useState(1);
+    const [isLoading, setIsLoading] = useState(false);
+
+    // Algorithm Parameters.
+    const [chosenAlgo, setChosenAlgo] = useState(null);
+    const [mu, setMu] = useState(0);
+    const [lambda, setLambda] = useState(0);
+
+    const router = useRouter();
+
+    return isLoading ? <Loader type={"full"} message={"Running Algorithm..."} /> : (
+        <main className="items-center justify-items-center min-h-screen font-[family-name:var(--font-geist-mono)] p-8">
+            <div>
+                <h1 className="text-3xl sm:text-4xl font-bold">
+                    Evolve OnClick
+                </h1>
+                <p>Run and Visualize algorithms with just a click.</p>
+            </div>
+
+            <div className="border border-gray-400 rounded-2xl p-4 min-w-[40%] max-w-[70%]">
+                <form className="flex flex-col">
+                    <h3 className="text-xl font-bold">Configure Algorithm</h3>
+                    <hr className="my-4" />
+
+                    {currentStep >= 1 && (
+                        <ChooseAlgo
+                            algoList={algorithmData}
+                            chosenAlgo={chosenAlgo}
+                            setChosenAlgo={setChosenAlgo}
+                            currentStep={currentStep}
+                            nextStep={2}
+                            setCurrentStep={setCurrentStep}
+                            mu={mu}
+                            setMu={setMu}
+                            lambda={lambda}
+                            setLambda={setLambda}
+                        />
+                    )}
+
+                    {currentStep >= 2 && (
+                        <ChooseWeights />
+                    )}
+                </form>
+            </div>
+        </main>
+    );
+}
