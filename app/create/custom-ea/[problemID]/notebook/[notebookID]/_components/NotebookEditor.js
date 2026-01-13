f424795120cb228d5184e84ce8fa4eb00cc2f198
Thanus Kumaar A <thanuskumaara@gmail.com>
2026-01-13 20:40:07 +0530

    Fix now working

--- app/create/custom-ea/[problemID]/notebook/[notebookID]/_components/NotebookEditor.js
+++ app/create/custom-ea/[problemID]/notebook/[notebookID]/_components/NotebookEditor.js
@@ -59,6 +59,7 @@ export default function NotebookEditor({ notebookId, problemId }) {
         session,
         setSession,
         startSessionRef,
+        fixCell,
         modifyCell,
         messages,
         addMessage,
